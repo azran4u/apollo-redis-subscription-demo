5332e77249674b5c20e77da1189fc63daef1be91
eyal <azran4u@gmail.com>
2021-04-18 00:24:38 +0300

    change eb node command

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ import { logger } from './utils';
 import { startExpressServer } from './app';
 
 (async () => {
-  const PORT = +process.env.GRAPHQL_PORT || 4000;
+  const PORT = +process.env.PORT || 4000;
   const { httpServer, apolloServer } = await startExpressServer();
   httpServer.listen(PORT, () => {
-    console.log(
+    logger.info(
       `🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`,
     );
-    console.log(
+    logger.info(
       `🚀 Subscriptions ready at ws://localhost:${PORT}${apolloServer.subscriptionsPath}`,
     );
   });
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,34 +1,39 @@
-import { createLogger, format, transports } from "winston";
+import { createLogger, format, transports } from 'winston';
 const { combine, timestamp, label, prettyPrint } = format;
 
 enum LoggerLevel {
-	error = "error",
-	warn = "warn",
-	info = "info",
-	verbose = "verbose",
-	debug = "debug",
-	silly = "silly"
+  error = 'error',
+  warn = 'warn',
+  info = 'info',
+  verbose = 'verbose',
+  debug = 'debug',
+  silly = 'silly',
 }
 const PROD_LOG_LEVEL = LoggerLevel.error;
 const DEV_LOG_LEVEL = LoggerLevel.debug;
 const LOG_FILE = LoggerLevel.error;
 
 export const logger = createLogger({
-	format: combine(
-		format.colorize(),
-		format.splat(),
-		format.simple(),
-		format.timestamp()
-	),
-	transports: [
-		new transports.Console({
-			level:
-				process.env.NODE_ENV === "production" ? PROD_LOG_LEVEL : DEV_LOG_LEVEL
-		}),
-		new transports.File({ filename: "/logs/error.log", level: LOG_FILE })
-	]
+  format: combine(
+    format.colorize(),
+    format.splat(),
+    format.simple(),
+    format.timestamp(),
+  ),
+  transports: [
+    new transports.Console({
+      level:
+        process.env.NODE_ENV === 'production'
+          ? PROD_LOG_LEVEL
+          : DEV_LOG_LEVEL,
+    }),
+    new transports.File({
+      filename: 'logs/error.log',
+      level: LOG_FILE,
+    }),
+  ],
 });
 
-if (process.env.NODE_ENV !== "production") {
-	logger.debug("Logging initialized at debug level");
+if (process.env.NODE_ENV !== 'production') {
+  logger.debug('Logging initialized at debug level');
 }
