38346557fd269edc161071980d893a99a1789574
eyal <azran4u@gmail.com>
2021-04-18 09:14:05 +0300

    add health endpoint

--- dist/routes.js
+++ dist/routes.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
-router.get('/ok', function (req, res) {
+router.get('/health', function (req, res) {
     res.status(200).send('ok');
 });
 exports.default = router;
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/ok', function (req, res) {
+router.get('/health', function (req, res) {
   res.status(200).send('ok');
 });
 
